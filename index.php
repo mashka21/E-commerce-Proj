0af04a7bf88d3b9a750bc721ef876b13d87a8bae
mashka21 <mashquul252@gmail.com>
2021-11-13 23:02:03 +0600

    plus media

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
     <link rel="stylesheet" href="css/owl.theme.default.min.css">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/styl.css">
+    <link rel="stylesheet" href="css/media.css">
 </head>
 <body>
     <div class="wrapper">
