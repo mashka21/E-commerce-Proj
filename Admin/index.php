d9d4efbb15b86b1b82a0f1da4898990f2ae85f2f
mashka21 <mashquul252@gmail.com>
2021-11-17 21:49:10 +0600

    admin panel-2

--- Admin/index.php
+++ Admin/index.php
@@ -57,7 +57,7 @@
                 <div class="row">
                     <!-- catogory part-->
                     <div class="col-md-3">
-                        <div class="card bg-danger text-white">
+                        <div class="card bg-orange-g text-white">
                             <div class="card-body">
                                 <h4 class="fw-light"><i class="fas fa-dolly"></i> All Catogory</h4>
                                 <hr>
@@ -69,7 +69,7 @@
                     </div>
                     <!-- catogory brand -->
                     <div class="col-md-3">
-                        <div class="card bg-success text-white">
+                        <div class="card bg-green-g text-white">
                             <div class="card-body">
                                 <h4 class="fw-light"><i class="fas fa-dolly-flatbed"></i> All Brands</h4>
                                 <hr>
@@ -81,7 +81,7 @@
                     </div>
                     <!-- catogory users -->
                     <div class="col-md-3">
-                        <div class="card bg-primary text-white">
+                        <div class="card bg-primary-g text-white">
                             <div class="card-body">
                                 <h4 class="fw-light"><i class="fas fa-users"></i> All User</h4>
                                 <hr>
@@ -90,7 +90,7 @@
                                 </h5>
                             </div>
                         </div>
-                    </div>
+                    </div> 
                     <!-- catogory orders -->
                     <div class="col-md-3">
                         <div class="card bg-info text-white">
@@ -105,6 +105,59 @@
                     </div>
 
 
+                </div>
+
+                <!-- all order table -->
+                <div class="All-order mt-5">
+                <h2>New Orders</h2>
+                <hr>
+                <table class="table table-bordered table-hover">
+                    <thead>
+                        <tr class="bg-primary text-white">
+                            <th scope="col">Order No.</th>
+                            <th scope="col">Order Name</th>
+                            <th scope="col">Date</th>
+                            <th scope="col">Paid Status</th>
+                            <th scope="col">Order Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>12/03/2021</td>
+                            <td><span class="badge bg-danger"> Unpaid</span></td>
+                            <td><span class="badge bg-warning"> No service</span></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>12/03/2021</td>
+                            <td ><span class="badge bg-success"> paid</span></td>
+                            <td><span class="badge bg-info"> process</span></td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td>Larry the Bird</td>
+                            <td>12/03/2021</td>
+                            <td><span class="badge bg-success"> paid</span> </td>
+                            <td><span class="badge bg-danger"> Rejected</span> </td>
+                        </tr>
+                    </tbody>
+                 </table>
+                </div>
+
+                <!-- pagination   -->
+                <div class="order-pagination">
+                <nav aria-label="Page navigation example">
+  <ul class="pagination">
+    <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+    <li class="page-item"><a class="page-link" href="#">1</a></li>
+    <li class="page-item"><a class="page-link" href="#">2</a></li>
+    <li class="page-item"><a class="page-link" href="#">3</a></li>
+    <li class="page-item"><a class="page-link" href="#">Next</a></li>
+  </ul>
+</nav>
                 </div>
             </div>
         </section>
